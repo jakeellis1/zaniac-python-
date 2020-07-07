7a475b9453a07b81ee2a04f11ad384ece0977464
jakeellis1 <67961709+jakeellis1@users.noreply.github.com>
2020-07-07 09:52:00 -0400

    Add files via upload

---
+++ Jake3.py
@@ -0,0 +1,20 @@
+#Jake Ellis
+#Tuesday day two
+#Blue
+
+print("You will glide across my floor with a cat on your back dang it stupid walls")
+
+Hours=1233344567876
+Gods =1376840436576
+
+print(Hours*Gods)
+
+print(Hours+Gods)
+
+print(Hours-Gods)
+
+print(Hours/Gods)
+
+print(Hours*Gods/12)
+
+print("You can kill me",Gods," Times.")
