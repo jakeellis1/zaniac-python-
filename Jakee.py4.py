64de0ffe0515685fac09810c3f72b3fd010da559
jakeellis1 <67961709+jakeellis1@users.noreply.github.com>
2020-07-08 11:56:57 -0400

    Add files via upload

---
+++ Jakee.py4.py
@@ -0,0 +1,29 @@
+# Jake Ellis
+# Wensday July 8th
+
+food= "Chicken fried rice"
+
+message= "I love to eat the %s it tastes great"
+
+print(message % food) 
+
+#Multiplication challange
+
+letter = "q"
+print(letter*3)
+
+#List challenge
+
+list1 = ['Hansel','Gretel','Oscar','Truly']
+list2 = ['Axe','sword','staff','chakram']
+
+list2.append('dagger')
+print(list2)
+
+del list2[4]
+print(list2) 
+
+print(list1)
+
+numlist =[7,8,9]
+print(numlist*8)
