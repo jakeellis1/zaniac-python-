d5a6af83b9aa936d1f2620e9f9fdf686fc5d7bf3
jakeellis1 <67961709+jakeellis1@users.noreply.github.com>
2020-07-09 10:15:54 -0400

    Add files via upload

---
+++ Jake 5.py
@@ -0,0 +1,25 @@
+#Jake Ellis
+#7/9/2020
+
+list1=["Asoro", "Facora", "Sprit","Human","Feinds"]
+list2=["Hapara","Whalevine","Rhititan","Vapporisin"]
+list1.append("Borin")
+print(list1)
+print(list2)
+del list1[5]
+print(list1)
+
+print(list1+list2)
+
+Merp = ('Cats','Dogs','Lizards','Bird')
+print(Merp[0])
+print(Merp[1],Merp[2])
+print(Merp[0:2])
+
+Map1= {'Oscar':'Crinkleballs', 'Truly': 'Mrsnowman', 'Hansel' : 'Catnipsurfboard','Gretel':'Baby'}
+del Map1['Oscar']
+print(Map1)
+Map1['Breadcrumbs']='Rainbowwand'
+print(Map1)
+Map1['Gretel']='Bagofcatnip'
+print(Map1)
