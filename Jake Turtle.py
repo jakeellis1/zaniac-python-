ada57dec3a948aec9a9b2e507ac46a3807185d4a
jakeellis1 <67961709+jakeellis1@users.noreply.github.com>
2020-07-09 11:37:38 -0400

    Add files via upload

---
+++ Jake Turtle.py
@@ -0,0 +1,21 @@
+#Turtle
+import turtle
+t = turtle.Pen()
+t.color(0,0,1)
+t.forward(200)
+t.left(90)
+t.forward(95)
+t.left(90)
+t.forward(200)
+t.left(90)
+t.forward(95)
+
+t.clear()
+t.color(1,0,0)
+
+t.left(90)
+t.forward(100)
+t.left(100)
+t.forward(100)
+t.left(90)
+t.forward(100)
