c0cd49991a05ed41a723b701af145a7d629ede65
jakeellis1 <67961709+jakeellis1@users.noreply.github.com>
2020-07-10 10:14:54 -0400

    Add files via upload

---
+++ Jake conditionals.py
@@ -0,0 +1,20 @@
+Deaths=1000
+if Deaths>=20:
+    Deaths= 'you good'
+elif Deaths>=50:
+    Deaths= 'you okay'
+elif Deaths>=100:
+    Deaths= 'you normal'
+elif Deaths>=500:
+    Deaths= 'you bad'
+elif Deaths>=1000:
+    Deaths='You worse'
+elif Deaths>=1500:
+    Deaths='You terrible'
+elif Deaths>=2000:
+    Deaths='You are horrible why did you play that game'
+print("Your rating of skill is:", Deaths)
+
+        
+
+         
---
+++ free draw.py
@@ -0,0 +1,47 @@
+import turtle
+t=turtle.Pen()
+t.color(0,0,1)
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.right(90)
+t.forward(450)
+t.clear()
+t.backward(480) 
+t.right(90)
+t.forward(200)
+t.backward(250)
+t.right(90)
+t.forward(200)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(150)
+t.right(90)
+t.forward(100)
+t.clear()
+for x in range(4):
+    t.forward(100)
+    t.left(90)
+for x in range(10):
+    t.right(90)
+    t.forward(200)
+for x in range(12):
+    t.left(90)
+    t.forward(300)
+t.clear()
+for x in range(10):
+    t.right(45)
+    t.forward(100)
+for x in range(12):
+    t.right(45)
+    t.forward(70)
+for x in range(20):
+    
+    t.right(50)
+    t.forward(55)
+    
+for x in range(10):
+    t.right(20)
+    t.forward(200)
+    
